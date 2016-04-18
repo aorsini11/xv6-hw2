5ef8ac227c25e77b3dc970634c85d26aaa3ca05d
Alessandro Orsini <ao272@pwd.cs.rutgers.edu>
2016-04-18 14:51:07 -0400

    added empty p_thread methods

--- pthread.h
+++ pthread.h
@@ -4,4 +4,19 @@
 // Define all functions and types for pthreads here.
 // This can be included in both kernel and user code.
 
+typedef struct
+{
+	int pid;
+	void *stack;
+} pthread_t;
+
+typedef struct
+{
+	void *attr;
+} pthread_attr_t;
+
+int pthread_create(pthread_t *thread, const pthread_attr_t *attr, void *(*start_routine) (void *),void *arg);
+int pthread_join(pthread_t thread, void **retval);
+int pthread_exit(void *retval);
+
 #endif
